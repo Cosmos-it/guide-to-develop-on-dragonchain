717bd9652e3468dc2e1039abaf6ddd65b27f7eb2
Adam Crowder <adam@adamcrowder.net>
2019-03-27 19:49:01 -0700

    more cleanup

--- contract-templates/c_contract/contract/handler.h
+++ contract-templates/c_contract/contract/handler.h
@@ -1,1 +1,6 @@
+#ifndef CONTRACT_H
+#define CONTRACT_H
+
 char* handler(char*);
+
+#endif
--- contract-templates/cpp_contract/contract/handler.cpp
+++ contract-templates/cpp_contract/contract/handler.cpp
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include "handler.h"
 
-using namespace std;
-
-string handler(string payload) {
+std::string handler(std::string payload) {
     fprintf(stderr, "This is a log\n");
-    string returnValue = "Hello from cpp smart contract: " + payload;
+    std::string returnValue = "Hello from cpp smart contract: " + payload;
     return returnValue;
 }
