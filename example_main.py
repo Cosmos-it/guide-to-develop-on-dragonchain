b6fe93994b2064c170c7d4290950cd0f54089550
Cosmos-it <cosmostaban@gmail.com>
2019-05-14 18:09:57 -0700

    Updates

--- example_main.py
+++ example_main.py
@@ -1,27 +1,27 @@
 import json
 import dragonchain_sdk
 
-client = dragonchain_sdk.Client(dragonchain_id='your_dc_id', auth_key_id='your_auth_key_id', auth_key='your_auth_key')
+client = dragonchain_sdk.Client(dragonchain_id='05b12c6d-41dc-4336-a932-d8b57ca5b04f', auth_key_id='FFMSSPXDBEVN', auth_key='ogdmoDOiNoND1j5fE6X2CEiVonfG5l48dWKlLOjMAhH')
 
 # Posting NodeJs contract
 print(json.dumps(client.post_contract(
-    txn_type='node_contract',
-    image='image_name',
+    txn_type='calculator',
+    image='taban/calculator',
     cmd='node',
     args=['index.js'],
     execution_order='parallel',
-    auth='<docker_auth_token_if_private_repository>'
+    # auth='<docker_auth_token_if_private_repository>'
 )))
 
 # Updating NodeJs contract
-print(json.dumps(client.update_contract(
-    contract_id='<contract_id>',
-    image='image_name',
-    cmd='node', 
-    args=['index.js'],
-    execution_order='parallel',
-    auth='<docker_auth_token_if_private_repository>'
-)))
+# print(json.dumps(client.update_contract(
+#     contract_id='<contract_id>',
+#     image='image_name',
+#     cmd='node', 
+#     args=['index.js'],
+#     execution_order='parallel',
+#     auth='<docker_auth_token_if_private_repository>'
+# )))
 
 # To post smart contract written in different languages, 
 # change the cmd and args values.
